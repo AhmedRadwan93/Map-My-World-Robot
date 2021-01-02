f0f10130d678d12bf4cc7f132c96dca10937367d
AhmedRadwan93 <ahmedelmaghwry93@gmail.com>
2021-01-02 11:34:45 +0000

    pushing the project

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/workspace/Mapping_my_world/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/workspace/Mapping_my_world/devel;/opt/ros/kinetic".split(';') if "/home/workspace/Mapping_my_world/devel;/opt/ros/kinetic" != "" else []
